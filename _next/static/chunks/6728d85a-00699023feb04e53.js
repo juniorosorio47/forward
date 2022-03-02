cbc57eb47c922b48b1caae192b91064ddc3a8e22
juniorosorio47 <juniorosorio47@users.noreply.github.com>
2022-03-02 18:16:15 +0000

    deploy: 311240f7d34a7daa0d97fef61f2e00f5e99cceff

---
+++ _next/static/chunks/6728d85a-00699023feb04e53.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[158],{5155:function(t,n,e){e.d(n,{QuR:function(){return u}});var r=e(8357);function u(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",stroke:"#000",strokeWidth:"2",d:"M2,7 L20,7 M16,2 L21,7 L16,12 M22,17 L4,17 M8,12 L3,17 L8,22"}}]})(t)}}}]);
